7ae5abf8b11d15f915b2c621818e35bf54732fa8
류제천 <bo_ryu@lyujecheon-ui-MacBookPro.local>
2022-09-21 15:08:53 +0900

    feat: Tailwind v3 도입

--- pages/components.js
+++ pages/components.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react components for routing our app without refresh
@@ -30,63 +30,97 @@ import SectionDownload from "/pages-sections/Components-Sections/SectionDownload
 
 import styles from "/styles/jss/nextjs-material-kit/pages/components.js";
 
+import YouTube from "react-youtube";
+import axios from "axios";
+
 const useStyles = makeStyles(styles);
 
 export default function Components(props) {
-  const classes = useStyles();
-  const { ...rest } = props;
-  return (
-    <div>
-      <Header
-        brand="NextJS Material Kit"
-        rightLinks={<HeaderLinks />}
-        fixed
-        color="transparent"
-        changeColorOnScroll={{
-          height: 400,
-          color: "white"
-        }}
-        {...rest}
-      />
-      <Parallax image="/img/nextjs_header.jpg">
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem>
-              <div className={classes.brand}>
-                <h1 className={classes.title}>NextJS Material Kit.</h1>
-                <h3 className={classes.subtitle}>
-                  A Badass Material Kit based on Material-UI and NextJS.
-                </h3>
-              </div>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
+    const classes = useStyles();
+    const { ...rest } = props;
+    const opts = {
+        height: "390",
+        width: "640",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+            rel: 0,
+            modestbranding: 1,
+        },
+    };
+    const apiAddress = "https://www.googleapis.com/youtube/v3/videos";
+    const snippet = {
+        channelId: "tvNDENT",
+    };
+    const apiKey = "AIzaSyAFn23qxwu-hXYwMztNn_-wAANMUpyU6Xo";
+    useEffect(async () => {
+        const data = await axios.get(`${apiAddress}?part=snippet&snippet=${snippet}&key=${apiKey}`, {
+            headers: {
+                authorization:
+                    "Bearer ya29.a0Aa4xrXNmIhT-yCw8GjQX0zGlOaZwiCC6hl97yzOXJhuRpgY-cp6wLUsHyoOrwo_jhAqmjNE4WNoq6qMbYLzlo93Yn02NtDq8jpnZTO5nu063yXvws16275vii5hoDGIyF1uboQZE2kQaa2E6lyQ5kxQVeHnGH7NOAFJsI60UaCgYKATASARMSFQEjDvL98DC_WjMyeBCK6_azqa-8dg0175",
+            },
+        });
+        console.log("data:", data);
+        return null;
+    }, []);
 
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <SectionBasics />
-        <SectionNavbars />
-        <SectionTabs />
-        <SectionPills />
-        <SectionNotifications />
-        <SectionTypography />
-        <SectionJavascript />
-        <SectionCarousel />
-        <SectionCompletedExamples />
-        <SectionLogin />
-        <GridItem md={12} className={classes.textCenter}>
-          <Link href="/login">
-            <a className={classes.link}>
-              <Button color="primary" size="lg" simple>
-                View Login Page
-              </Button>
-            </a>
-          </Link>
-        </GridItem>
-        <SectionExamples />
-        <SectionDownload />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header
+                brand="NextJS Material Kit"
+                rightLinks={<HeaderLinks />}
+                fixed
+                color="transparent"
+                changeColorOnScroll={{
+                    height: 400,
+                    color: "white",
+                }}
+                {...rest}
+            />
+            <YouTube
+                videoId="r3RGVcu_ZI0"
+                opts={opts}
+                onEnd={(e) => {
+                    e.target.stopVideo(0);
+                }}
+            />
+            <Parallax image="/img/nextjs_header.jpg">
+                <div className={classes.container}>
+                    <GridContainer>
+                        <GridItem>
+                            <div className={classes.brand}>
+                                <h1 className={classes.title}>NextJS Material Kit.</h1>
+                                <h2 className={classes.subtitle}>A Badass Material Kit based on Material-UI and NextJS.</h2>
+                            </div>
+                        </GridItem>
+                    </GridContainer>
+                </div>
+            </Parallax>
+
+            <div className={classNames(classes.main, classes.mainRaised)}>
+                <SectionBasics />
+                <SectionNavbars />
+                <SectionTabs />
+                <SectionPills />
+                <SectionNotifications />
+                <SectionTypography />
+                <SectionJavascript />
+                <SectionCarousel />
+                <SectionCompletedExamples />
+                <SectionLogin />
+                <GridItem md={12} className={classes.textCenter}>
+                    <Link href="/login">
+                        <a className={classes.link}>
+                            <Button color="primary" size="lg" simple>
+                                View Login Page
+                            </Button>
+                        </a>
+                    </Link>
+                </GridItem>
+                <SectionExamples />
+                <SectionDownload />
+            </div>
+            <Footer />
+        </div>
+    );
 }
